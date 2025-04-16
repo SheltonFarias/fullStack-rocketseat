d264ab3230cf87396086b686b06a6ae73f3f36cc
sheltonFarias <sheltonm10@outlook.com>
2025-04-16 15:53:15 -0300

    teste

---
+++ modulo16-Projeto_Convert/scripts.js
@@ -0,0 +1,33 @@
+// cotação de moedas do dia
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
+
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+
+// Manipulando o input amout para receber somente números
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g;
+  amount.value = amount.value.replace(hasCharactersRegex, "");
+});
+
+// capturando o evento de submit no formulário
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+  }
+};
+
+// função para converter a moeda.
+function convertCurrency(amount, price, symbol) {}
