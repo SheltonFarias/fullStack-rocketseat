b277a632d9ae4c8b20ce1ddce075e2dfea82223a
sheltonFarias <sheltonm10@outlook.com>
2025-04-30 12:47:57 -0300

    finish classroom 08

--- modulo18-Projeto Refund/scripts.js
+++ modulo18-Projeto Refund/scripts.js
@@ -1,6 +1,8 @@
 // Seleciona os elementos do formulario
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
-
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Captura o evento de input para formatar o valor.
 amount.oninput = () => {
@@ -12,13 +14,29 @@ amount.oninput = () => {
   
   // Atualiza do valor do input.
   amount.value = formatCurrencyBRL(value)
-
 }
 
 function formatCurrencyBRL(value) {
+  // Formata o valor no padrão BRL (Real Brasileiro)
   value = value.toLocaleString("pt-BR", {
     style: "currecy",
     curency: "BRL"
   })
   return value
+}
+
+form.onsubmit = (event) => {
+  // Previne o comportamento padrão de recarregar a pagina
+  event.preventDefault()
+
+  // cria um objeto com os detalhes da nova despesa
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount:amount.value,
+    created_at: new Date(),
+  }
+
 }
