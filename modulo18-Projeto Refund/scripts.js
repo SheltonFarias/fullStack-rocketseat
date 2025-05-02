1c34fca6b4751680ed619ad0f3b6ce413d8e369f
sheltonFarias <sheltonm10@outlook.com>
2025-05-02 13:00:47 -0300

    finish classroom 12

--- modulo18-Projeto Refund/scripts.js
+++ modulo18-Projeto Refund/scripts.js
@@ -1,33 +1,36 @@
 // Seleciona os elementos do formulario
-const form = document.querySelector("form")
-const amount = document.getElementById("amount")
-const expense = document.getElementById("expense")
-const category = document.getElementById("category")
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
+// Selecion os elementos da lista
+const expenseList = document.querySelector("ul")
 
 // Captura o evento de input para formatar o valor.
 amount.oninput = () => {
   // Obtem o valor atual do input e remove os caracteres não numericos.
-  let value = amount.value.replace(/\D/g, "")
+  let value = amount.value.replace(/\D/g, "");
 
   // Transformar o valor em centavos. (exemplo: 150/100 = 1.5 que é equivalente a R$ 1, 50).
-  value = Number(value) / 100
-  
+  value = Number(value) / 100;
+
   // Atualiza do valor do input.
-  amount.value = formatCurrencyBRL(value)
-}
+  amount.value = formatCurrencyBRL(value);
+};
 
 function formatCurrencyBRL(value) {
   // Formata o valor no padrão BRL (Real Brasileiro)
   value = value.toLocaleString("pt-BR", {
     style: "currecy",
-    curency: "BRL"
-  })
-  return value
+    curency: "BRL",
+  });
+  return value;
 }
 
 form.onsubmit = (event) => {
   // Previne o comportamento padrão de recarregar a pagina
-  event.preventDefault()
+  event.preventDefault();
 
   // cria um objeto com os detalhes da nova despesa
   const newExpense = {
@@ -35,25 +38,32 @@ form.onsubmit = (event) => {
     expense: expense.value,
     category_id: category.value,
     category_name: category.options[category.selectedIndex].text,
-    amount:amount.value,
+    amount: amount.value,
     created_at: new Date(),
-  }
+  };
 
-    // chama a função que ira adicionar o item na lista
-    expanseAdd(newExpense)
-}
+  // chama a função que ira adicionar o item na lista
+  expanseAdd(newExpense);
+};
 
 function expanseAdd(newExpense) {
   try {
-    // cria o elemento de li para adicionar o item (li) na lista(ul).   
-    const expenseItem = document.createElement("li")
-    expenseItem.classList.add("expense")
+    // cria o elemento de li para adicionar o item (li) na lista(ul).
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
 
     // cria o ícone da categoria.
-    const expanseIcon = document.createElement("img")
+    const expanseIcon = document.createElement("img");
+    expanseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+    expanseIcon.setAttribute("alt", newExpense.category_name)
 
+    // Adiciona as informações mo item.
+    expenseItem.append(expanseIcon)
+
+    // Adicona o item na lista.
+    expenseList.append(expenseItem)
   } catch (error) {
-    alert("Não foi possivel atualizar a lista de despesas")
-    console.log(error)
+    alert("Não foi possivel atualizar a lista de despesas");
+    console.log(error);
   }
 }
