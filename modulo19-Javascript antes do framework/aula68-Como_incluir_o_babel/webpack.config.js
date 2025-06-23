df5df48ed1b8483de9644f7e6916e4b92533d5d8
sheltonFarias <sheltonm10@outlook.com>
2025-06-23 13:57:15 -0300

    finish classroom 68

---
+++ modulo19-Javascript antes do framework/aula68-Como_incluir_o_babel/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("HTML-webpack-puglin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "js", "index.js"),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  plugins: [new HTMLWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        exclude: "/node_modules",
+      },
+      {
+        test: /\.js$/i,
+        exclude: "/node_modules",
+        use:{
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults"}]]
+          }
+        }
+      }
+    ],
+  },
+};
+
+// n
