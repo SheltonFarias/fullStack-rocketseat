79982b6bac7f0080ec7013dd4de1acbc1cf7ca0e
sheltonFarias <sheltonm10@outlook.com>
2025-10-03 12:58:05 -0300

    ok

--- modulo23-API-REST-com-node/api-rest/src/server.ts
+++ modulo23-API-REST-com-node/api-rest/src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express"
 import { routes } from "./routes"
+import { AppError } from "./utils/AppError"
 
 const PORT = 3333
 
@@ -24,9 +25,12 @@ app.use(routes)
 /**
  * 400 (Bad Request): Erro do cliente.
  * 500 (Internal Server Erro): Erro interno do Servidor.
- * 
  */
+
 app.use((error: any, request: Request, response: Response, _: NextFunction) => {
+  if(error instanceof AppError){
+    return response.status(error.statusCode).json({ message: error.message})
+  }
   response.status(500).json({ message: error.message})
  })
 
--- modulo23-API-REST-com-node/api-rest/src/utils/AppError.ts
+++ modulo23-API-REST-com-node/api-rest/src/utils/AppError.ts
@@ -1,5 +1,11 @@
 class AppError {
+  message: string
+  statusCode: number
 
+  constructor(message:string, statusCode:number = 400){
+    this.message = message
+    this.statusCode = statusCode
+  }
 }
 
 export { AppError }
