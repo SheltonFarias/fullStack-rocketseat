20daefbd3ec45855a033fb25c3322499e38cab51
sheltonFarias <sheltonm10@outlook.com>
2025-10-20 12:49:02 -0300

    finish classroom 14

--- modulo26-criando-api-de-restaurante/src/server.ts
+++ modulo26-criando-api-de-restaurante/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express"
 
 import { routes } from "./routes"
+import { errorHandling } from "./middlewares/error-handling"
 
 const PORT = 3333
 
@@ -8,5 +9,7 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use(errorHandling)
 
 app.listen(PORT, () => console.log(`server on http://localhost:${PORT}`))
