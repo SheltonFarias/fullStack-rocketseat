b21d6e3c6ac7aebb36a0d51ca04ead5e21feb655
SheltonFarias <sheltonm10@outlook.com>
2026-01-06 13:51:39 -0300

    finish Classroom 14 - validando dados do produto

--- modulo26-criando-api-de-restaurante/src/controllers/products-controller.ts
+++ modulo26-criando-api-de-restaurante/src/controllers/products-controller.ts
@@ -1,14 +1,28 @@
 import { NextFunction, Request, Response } from "express";
-import { AppError } from "../utils/AppError";
+import { z } from "zod"
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
-      return response.json({ message: "Ok" })
+      return response.json({ message: "Ok" });
     } catch (error) {
-      next(error)
+      next(error);
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0, { message: "value must be greater than 0" }),
+      })
+
+      const { name, price } = request.body
+      return response.status(201).json({ message: "Created", name, price });
+    } catch (error) {
+      next(error);
     }
   }
 }
 
-export { ProductController }
+export { ProductController };
