71ea6049965efb423fbe689fd7619345bacc44f8
SheltonFarias <sheltonm10@outlook.com>
2026-03-04 12:37:59 -0300

    correct commit

--- modulo26-criando-api-de-restaurante/src/controllers/products-controller.ts
+++ modulo26-criando-api-de-restaurante/src/controllers/products-controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
-import { knex } from "@/database/knex"
-import { z } from "zod"
+import { knex } from "@/database/knex";
+import { z } from "zod";
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
@@ -16,9 +16,9 @@ class ProductController {
       const bodySchema = z.object({
         name: z.string().trim().min(6),
         price: z.number().gt(0, { message: "value must be greater than 0" }),
-      })
+      });
 
-      await knex("products").insert()
+      await knex<ProductRepository>("products").insert({ name, price, other });
 
       return response.status(201).json({ message: "Created", name, price });
     } catch (error) {
