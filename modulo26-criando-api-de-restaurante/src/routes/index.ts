09b46f87ddab4c02d0a110a5d1d92298d6afed42
SheltonFarias <sheltonm10@outlook.com>
2026-03-05 12:54:22 -0300

    finish class 25 module26

--- modulo26-criando-api-de-restaurante/src/routes/index.ts
+++ modulo26-criando-api-de-restaurante/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { tablesSessionsRoutes } from "./table-sessions-routes";
 import { productsRoutes } from "./products-routes";
 import { tablesRoutes } from "./table-routes";
 
@@ -7,5 +8,6 @@ import { tablesRoutes } from "./table-routes";
 const routes = Router()
 routes.use("/products", productsRoutes)
 routes.use("/tables", tablesRoutes)
+routes.use("tables-sessions", tablesSessionsRoutes)
 
 export { routes }
