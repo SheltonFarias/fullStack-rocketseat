aa61307236e0fdfbdc27c547a5112d4ca9aca5e4
SheltonFarias <sheltonm10@outlook.com>
2026-03-13 16:50:39 -0300

    finish module29

--- modulo29-autenticacao-e-autorizacao/auth/src/controllers/sessions-controller.ts
+++ modulo29-autenticacao-e-autorizacao/auth/src/controllers/sessions-controller.ts
@@ -1,9 +1,29 @@
+import { AppError } from "@/utils/AppError"
 import { Request, Response } from "express"
+import { authConfig } from "../../configs/auth"
+import { sign } from "jsonwebtoken"
 
 class SessionsController {
   async create(request: Request, response: Response) {
+    const { username, password } = request.body
 
-    return response.json({ message: "ok" })
+    const fakeUser = {
+      id: "1",
+      username: "testse",
+      password: "1234567",
+      role: "customer"
+    }
+
+    if(username !== fakeUser.username || password !== fakeUser.password ) {
+      throw new AppError("Username ou senha incorreta", 401)
+    }
+    const{secret, expiresIn} = authConfig.jwt
+    const token = sign({role: fakeUser.role},secret, {
+      expiresIn,
+      subject: String(fakeUser.id),
+    } )
+
+    return response.json({ message: token })
   }
 }
 
