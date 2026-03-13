1ccfef0b4f7400a6c56038e9fd82ef3812e4c28e
SheltonFarias <sheltonm10@outlook.com>
2026-03-13 16:54:04 -0300

    add comment

--- modulo29-autenticacao-e-autorizacao/auth/src/routes/products.routes.ts
+++ modulo29-autenticacao-e-autorizacao/auth/src/routes/products.routes.ts
@@ -7,7 +7,12 @@ import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization"
 const productsRoutes = Router()
 const productsController = new ProductsController()
 
+// Aplicar autorização em todas as rota a seguir
+// productsRoutes.use(verifyUserAuthorization(["sale", "admin"]))
+
 productsRoutes.get("/", productsController.index)
+
+// autorização em um rota especifica
 productsRoutes.post(
   "/",
   ensureAuthenticated,
