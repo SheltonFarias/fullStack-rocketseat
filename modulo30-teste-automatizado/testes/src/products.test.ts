41e1b7be47cd95f417bf847519e33a5e2bd8c6e5
SheltonFarias <sheltonm10@outlook.com>
2026-03-17 12:40:51 -0300

    finish module30

--- modulo30-teste-automatizado/testes/src/products.test.ts
+++ modulo30-teste-automatizado/testes/src/products.test.ts
@@ -4,7 +4,9 @@ import { app } from "./app"
 describe("products", () => {
   it("should list product", async () => {
     const response = await request(app).get("/products")
-    console.log(response.body)
+    
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toHaveLength(3)
   })
 })
 
