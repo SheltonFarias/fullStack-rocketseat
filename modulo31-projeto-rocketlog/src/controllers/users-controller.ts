7fca6952495bb328b0e69048ea51e44eafa3ec3e
SheltonFarias <sheltonm10@outlook.com>
2026-03-30 14:13:01 -0300

    finish class 18 module31

--- modulo31-projeto-rocketlog/src/controllers/users-controller.ts
+++ modulo31-projeto-rocketlog/src/controllers/users-controller.ts
@@ -1,8 +1,38 @@
 import { Request, Response, NextFunction } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/database/prisma"
+import { hash } from "bcrypt"
+import { z } from "zod"
 
 class UserController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok" })
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.string().email(),
+      password: z.string().min(6),
+    })
+
+    const { name, email, password } = bodySchema.parse(request.body)
+
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } })
+
+    const hashedPassword = await hash(password, 8)
+
+    if(userWithSameEmail) {
+      throw new AppError("user with same email already exists")
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+
+    const { password:_, ...userWithoutPassword} = user
+
+    return response.json(userWithoutPassword)
   }
 }
 
