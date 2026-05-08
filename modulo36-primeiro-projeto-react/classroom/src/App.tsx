6482f3b956de969525bb4bae1a4f7d8bb764781c
SheltonFarias <sheltonm10@outlook.com>
2026-05-08 17:05:02 -0300

    finish class 9 module36

--- modulo36-primeiro-projeto-react/classroom/src/App.tsx
+++ modulo36-primeiro-projeto-react/classroom/src/App.tsx
@@ -1,10 +1,16 @@
-import "./App.css";
+import styles from "./App.module.css";
 import { Header } from "./components/Header";
+import { Tip } from "./components/Tip";
 
 function App() {
+  function handleRestartGame() {
+    alert("Reiniciar o jogo!");
+  }
+
   return (
-    <div>
-      <Header/>
+    <div className={styles.container}>
+      <Header current={5} max={10} onRestart={handleRestartGame} />
+      <Tip tip="Uma Linguagens de programação dinâmica" />
     </div>
   );
 }
