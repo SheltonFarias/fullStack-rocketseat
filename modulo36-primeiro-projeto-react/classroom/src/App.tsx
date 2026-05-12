e0ef472c4ceeb52c2f59ca9abe65189599f9ae9a
SheltonFarias <sheltonm10@outlook.com>
2026-05-11 23:02:06 -0300

    finish class 19 module 36

--- modulo36-primeiro-projeto-react/classroom/src/App.tsx
+++ modulo36-primeiro-projeto-react/classroom/src/App.tsx
@@ -1,16 +1,64 @@
 import styles from "./App.module.css";
-import { Header } from "./components/Header";
+import { useEffect, useState } from "react";
+
+import { WORDS, Challenge } from "./utils/words";
+
 import { Tip } from "./components/Tip";
+import { Input } from "./components/Input";
+import { Button } from "./components/Button";
+import { Letter } from "./components/Letter";
+import { Header } from "./components/Header";
+import { LettersUsed, LetterUsedProps } from "./components/LettersUsed";
 
 function App() {
+  const [letter, setLetter] = useState("");
+  const [attempts, setAttempts] = useState(0);
+  const [lettersUsed, setLetterUsed] = useState<LetterUsedProps>([]);
+  const [challenge, setChallenge] = useState<Challenge | null>(null);
+
   function handleRestartGame() {
     alert("Reiniciar o jogo!");
   }
 
+  function startGame() {
+    const index = Math.floor(Math.random() * WORDS.length);
+    const randomWord = WORDS[index];
+    setChallenge(randomWord);
+
+    setAttempts(0);
+    setLetter("");
+  }
+
+  useEffect(() => {
+    startGame();
+  }, []);
+
+  if (!challenge) {
+    return;
+  }
+
   return (
     <div className={styles.container}>
-      <Header current={5} max={10} onRestart={handleRestartGame} />
-      <Tip tip="Uma Linguagens de programação dinâmica" />
+      <main>
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
+
+        <Tip tip="Uma Linguagens de programação dinâmica" />
+
+        <div className={styles.word}>
+          {challenge.word.split("").map(() => (
+            <Letter value="" />
+          ))}
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div className={styles.guess}>
+          <Input autoFocus maxLength={1} placeholder="?" />
+          <Button title="Confirmar" />
+        </div>
+
+        <LettersUsed data={lettersUsed} />
+      </main>
     </div>
   );
 }
---
+++ modulo36-primeiro-projeto-react/classroom/src/components/LettersUsed/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./styles.module.css";
+
+import { Letter } from "../Letter";
+
+export type LetterUsedProps = {
+  value: string;
+  correct: boolean;
+};
+
+type Props = {
+  data: LetterUsedProps[];
+};
+
+export function LettersUsed([data]: Props) {
+  return (
+    <div className={styles.lettersUsed}>
+      <h5>Letras Utilizadas</h5>
+
+      <div>
+        {data.map(({ value, correct }) => (
+          <Letter value={value} size="small" color={correct ? "correct" : "wrong"} />
+        ))}
+      </div>
+    </div>
+  );
+}
