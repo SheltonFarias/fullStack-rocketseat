f59485bc71cfe36cf0fd9e0a7588c2d959f09f0f
SheltonFarias <sheltonm10@outlook.com>
2026-05-21 14:28:58 -0300

    start module 39

---
+++ modulo38-formulario-em-react/forms-template/codigos-exemplos.md
@@ -0,0 +1,50 @@
+
+````
+import "./App.css";
+
+import { useState } from "react";
+import {Control, useForm} from "react-hook-form"
+
+export default function App() {
+  const [name, setName] = useState("");
+
+  function onSubmit(e: React.FormEvent<HTMLElement>) {
+    e.preventDefault()
+  }
+
+  return (
+    <div>
+      <h1>Evento</h1>
+
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Nome do evento"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <span className="error">Nome é obrigatório</span>
+
+        <input type="date" placeholder="Nome do evento" lang="pt-BR"  value={name}/>
+
+        <select defaultValue="">
+          <option value="" disabled>
+            Selecione...
+          </option>
+
+          <option value="technology">React</option>
+          <option value="entertainment">Node.js</option>
+          <option value="business">Javascript</option>
+          <option value="business">Typescript</option>
+        </select>
+
+        <textarea placeholder="Descrição" rows={4} />
+
+        <button type="submit" onClick={onSubmit}>
+          Salvar
+        </button>
+      </form>
+    </div>
+  );
+}`
+
+`````
---
+++ modulo38-formulario-em-react/forms-template/src/App.tsx
@@ -0,0 +1,47 @@
+import "./App.css";
+
+import { Controller, useForm } from "react-hook-form";
+
+export default function App() {
+  const { control, handleSubmit } = useForm({defaultValues:{
+    name: "",
+  }});
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
+
+  return (
+    <div>
+      <h1>Evento</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="name"
+          render={({field}) => <input type="text" placeholder="Nome do evento" {...field}/>}
+        />
+        <input type="text" placeholder="Nome do evento" />
+        <span className="error">Nome é obrigatório</span>
+
+        <input type="date" placeholder="Nome do evento" lang="pt-BR" />
+
+        <select defaultValue="">
+          <option value="" disabled>
+            Selecione...
+          </option>
+
+          <option value="technology">React</option>
+          <option value="entertainment">Node.js</option>
+          <option value="business">Javascript</option>
+          <option value="business">Typescript</option>
+        </select>
+
+        <textarea placeholder="Descrição" rows={4} />
+
+        <button type="submit">Salvar</button>
+      </form>
+    </div>
+  );
+}
