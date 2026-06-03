8dcd5aed7f437972a88aa967c6341a7e4a8dc845
SheltonFarias <sheltonm10@outlook.com>
2026-06-03 16:38:24 -0300

    okkk

---
+++ modulo39-tailwind/web/src/pages/Refund.tsx
@@ -0,0 +1,5 @@
+export function Refund() {
+return(
+  <h1>teste</h1>
+)
+}
--- modulo39-tailwind/web/src/pages/Signup.tsx
+++ modulo39-tailwind/web/src/pages/Signup.tsx
@@ -2,18 +2,27 @@ import { useState } from "react";
 import { Input } from "../components/input";
 import { Button } from "../components/Button";
 
-export function SignIn() {
+export function SignUp() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
   const [isLoading, setIsloading] = useState(false);
 
   function onSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log(email, password);
+    console.log(name, email, password, passwordConfirm);
   }
 
   return (
     <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+      <Input
+        required
+        legend="Name"
+        placeholder="seu Nome"
+        onChange={(e) => setName(e.target.value)}
+      />
+
       <Input
         type="email"
         required
@@ -21,16 +30,25 @@ export function SignIn() {
         placeholder="seu@email.com"
         onChange={(e) => setEmail(e.target.value)}
       />
+
       <Input
         required
-        legend="Senha"
+        legend="Confirme sua Senha"
         type="password"
         placeholder="123456"
         onChange={(e) => setPassword(e.target.value)}
       />
 
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="123456"
+        onChange={(e) => setPasswordConfirm(e.target.value)}
+      />
+
       <Button type="submit" isLoading={isLoading}>
-        Entrar
+        Cadastrar
       </Button>
 
       <a
