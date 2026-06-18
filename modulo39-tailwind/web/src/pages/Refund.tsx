9aedeb8f0955bb7bb4a6062fcbc83446293ddcb6
SheltonFarias <sheltonm10@outlook.com>
2026-06-18 15:37:30 -0300

    okkkkk

--- modulo39-tailwind/web/src/pages/Refund.tsx
+++ modulo39-tailwind/web/src/pages/Refund.tsx
@@ -1,5 +1,23 @@
+import { Input } from "../components/input";
+import { Select } from "../components/select";
+
 export function Refund() {
-return(
-  <h1>teste</h1>
-)
+  return (
+    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+      <header>
+        <h1 className="text-xl font-bold text-gra-gray-100">
+          Solicitação de Reembolso
+        </h1>
+        <p className="text-sm text-gray-200 mt-2 mb-4">
+          Dados da despesa para solictar Reembolso.
+        </p>
+      </header>
+
+      <Input required legend="Nome da solicitação" />
+      <Select required legend="Categoria">
+        <option value="">Teste 1</option>
+        <option value="">Teste 2</option>
+      </Select>
+    </form>
+  );
 }
