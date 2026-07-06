526b798320d3fa388df62ba33d647072227aa0aa
SheltonFarias <sheltonm10@outlook.com>
2026-07-06 16:36:56 -0300

    okokok

---
+++ modulo39-tailwind/web/src/pages/Dashboard.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+import searchSvg from "../assets/search.svg";
+import { RefundItem } from "../components/RefundItem";
+import { CATEGORIES } from "../utils/categories";
+
+const REFUND_EXEMPLE = {
+  id: "123",
+  name: "Rodrigo",
+  category: "Transporte",
+  amount: "R$ 34,5",
+  categoryImg: CATEGORIES["transport"].icon
+}
+
+export function Dashboard() {
+  const [name, setName] = useState("");
+
+  function fetchRefunds(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(name);
+  }
+
+  return (
+    <div className="b-gray-500 rounded-xl p-10 md:min-w-[768px]">
+      <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
+
+      <form
+        onSubmit={fetchRefunds}
+        className="flex items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6"
+      >
+        <Input
+          placeholder="Pesquisar pelo nome"
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <Button type="submit" variant="icon">
+          <img src={searchSvg} alt="Ícone de pesquisar" className="w-5" />
+        </Button>
+      </form>
+      <div>
+        <RefundItem data={REFUND_EXEMPLE}/>
+      </div>
+    </div>
+  );
+}
